54a885aae96304565256cf1c83fc4e31c7564990
Botahamec <botahamec@outlook.com>
2020-06-20 15:22:07 -0400

    Created the generate_list function
    Randomly generates a list of a specified type and length

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rand::prelude::*;
+use rand::distributions;
+
+fn generate_list<T>(length: usize) -> Vec<T>
+		where distributions::Standard: distributions::Distribution<T> {
+	let mut list = Vec::<T>::with_capacity(length);
+	let mut rng = rand::thread_rng();
+
+	for _i in 0..length {list.push(rng.gen());}
+
+	list
+}
+
 fn main() {
-    println!("Hello, world!");
+	println!("Hello, world!");
 }
